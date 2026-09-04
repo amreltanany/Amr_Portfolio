6da4adc7b54b6ffa8b368275b53448ec22ce7e6f
Amr Tanany <tanany95175325@gmail.com>
2026-09-05 02:57:26 +0300

    Update base path to Amr_Portfolio to match repository name

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { fileURLToPath, URL } from 'node:url';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/new_port/',
+  base: '/Amr_Portfolio/',
   plugins: [react()],
   resolve: {
     alias: {
